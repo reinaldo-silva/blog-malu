c492705edac82b17d5089735f44cd5d4075e802d
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2022-04-15 20:40:54 -0300

    fix bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return <h1>Hello world</h1>;
